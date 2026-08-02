4325124d485707fc55c813b8dc18314db7d0d69d
Kapps <1259333+Kapps@users.noreply.github.com>
2026-08-02 07:14:46 +0000

    Continue Linux startup after audio warning

--- src/Weavie.Linux/Program.cs
+++ src/Weavie.Linux/Program.cs
@@ -4,15 +4,15 @@ using Weavie.Linux.Native;
 Gtk.gtk_init(IntPtr.Zero, IntPtr.Zero);
 
 const string missingAudioSink =
-	"Weavie needs GStreamer's 'autoaudiosink' element. Install GStreamer Good Plug-ins, then restart Weavie.\n\n"
+	"Weavie may freeze or crash because GStreamer's 'autoaudiosink' element is missing. "
+	+ "Install GStreamer Good Plug-ins, then restart Weavie.\n\n"
 	+ "Debian/Ubuntu: gstreamer1.0-plugins-good\nFedora: gstreamer1-plugins-good\nArch: gst-plugins-good";
 if (!GStreamer.HasAutoAudioSink()) {
 	IntPtr dialog = Gtk.gtk_message_dialog_new(
-		IntPtr.Zero, Gtk.DialogModal, Gtk.MessageError, Gtk.ButtonsClose, missingAudioSink);
+		IntPtr.Zero, Gtk.DialogModal, Gtk.MessageWarning, Gtk.ButtonsOk, missingAudioSink);
 	Gtk.gtk_window_set_title(dialog, "Missing Linux dependency");
 	_ = Gtk.gtk_dialog_run(dialog);
 	Gtk.gtk_widget_destroy(dialog);
-	return 1;
 }
 
 var host = new WorkspaceHost();
